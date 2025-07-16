fbb144cf7f8a8fee075b6260abd3f2c555230c58
ganta0131 <ganta0131@gmail.com>
2025-07-17 08:14:57 +0900

    Update: レシピ生成のHTML構造の修正

--- app.py
+++ app.py
@@ -79,8 +79,8 @@ def get_meal_plan():
 
         # レシピと買い物リストをHTML形式に変換
         paragraphs = meal_plan.split('\n')
-        recipes_html = '<div class="recipes">'
-        shopping_list_html = '<div class="shopping-list">'
+        recipes_html = ''
+        shopping_list_html = ''
         
         is_shopping_list = False
         current_day = None
@@ -107,15 +107,20 @@ def get_meal_plan():
                 continue
                 
             if is_shopping_list:
+                if not shopping_list_html:
+                    shopping_list_html = '<div class="shopping-list">'
                 shopping_list_html += f'<p>{para.strip()}</p>'
             else:
+                if not recipes_html:
+                    recipes_html = '<div class="recipes">'
                 recipes_html += f'<div class="recipe-item">{para.strip()}</div>'
         
         if current_day:
             recipes_html += '</div>'
         
         recipes_html += '</div>'
-        shopping_list_html += '</div>'
+        if shopping_list_html:
+            shopping_list_html += '</div>'
         
         return jsonify({
             'success': True,
